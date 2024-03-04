1652c113c5ef02cacdd341ae35f211dd68edcc89
Juan Ruben Nolasco Silva <jrnolasco@gmail.com>
2024-03-04 11:44:27 -0600

    first commit

---
+++ src/main/java/com/example/springbootdockersupport/SpringBootDockerSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockersupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdockersupport/SpringBootDockerSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockersupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
